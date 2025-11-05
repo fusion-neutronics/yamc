9ef075457aa9bf3dc002af5b5d5e29b61177e5da
Jon Shimwell <mail@jshimwell.com>
2025-11-05 22:46:06 +0000

    updated tests with q value

--- tests/test_reaction.py
+++ tests/test_reaction.py
@@ -6,8 +6,9 @@ def test_reaction_instantiation():
         ReactionProduct("neutron", "emission", 0.0),
         ReactionProduct("neutron", "emission", 0.0)
     ]
-    r = Reaction(products, [1.23], 0, [0], 18, [0.0, 1.0, 2.0])
+    r = Reaction(products, [1.23], 0, [0], 18, 0.0, [0.0, 1.0, 2.0])
     assert [p.particle for p in r.products] == ["neutron", "neutron"]
     assert r.mt_number == 18
     assert r.cross_section == [1.23]
+    assert r.q_value == 0.0
     assert r.energy_grid == [0.0, 1.0, 2.0]
