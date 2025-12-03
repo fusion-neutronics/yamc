894aaed9472adc6ace40ba3e02fda5b828df9954
shimwell <mail@jshimwell.com>
2025-12-03 17:46:41 +0100

    merged example

--- examples/python/tbr.py
+++ examples/python/tbr.py
@@ -1,76 +1,106 @@
 import time
-import materials_for_mc as mc
+
+
+for code in ['openmc', 'materials_for_mc']:
+    if code == 'openmc':
+        import openmc as mc
+        # mc.config['cross_sections'] = "/home/jon/nuclear_data/tendl-2021-hdf5/tendl-2021-hdf5/cross_sections.xml"
+        mc.config['cross_sections'] = "/home/jon/nuclear_data/cross_sections.xml"
+    elif code == 'materials_for_mc':
+        import materials_for_mc as mc
+
     
-# Create two-cell geometry: inner sphere (Li6) and outer annular region (Be9)
-sphere1 = mc.Sphere(
-    surface_id=1,
-    x0=0.0,
-    y0=0.0,
-    z0=0.0,
-    r=1.0,
-    boundary_type='transmission',
-)
-sphere2 = mc.Sphere(
-    surface_id=2,
-    x0=0.0,
-    y0=0.0,
-    z0=0.0,
-    r=200.0,
-    boundary_type='vacuum',
-)
-region1 = -sphere1
-region2 = +sphere1 & -sphere2
+    # Create two-cell geometry: inner sphere (Li6) and outer annular region (Be9)
+    sphere1 = mc.Sphere(
+        surface_id=1,
+        x0=0.0,
+        y0=0.0,
+        z0=0.0,
+        r=1.0,
+        boundary_type='transmission',
+    )
+    sphere2 = mc.Sphere(
+        surface_id=2,
+        x0=0.0,
+        y0=0.0,
+        z0=0.0,
+        r=200.0,
+        boundary_type='vacuum',
+    )
+    region1 = -sphere1
+    region2 = +sphere1 & -sphere2
 
-# Create materials with different absorption characteristics
-material1 = mc.Material()
-material1.material_id = 1  # Set material_id for MaterialFilter testing
-material1.add_nuclide("Li6", 0.07/2)  # Li4SiO4
-material1.add_nuclide("Li7", 0.93/2)
-material1.add_nuclide("Be9", 0.5)
-# material1.add_element("O", 4.0)
-# material1.add_element("Si", 1.0)
-material1.set_density("g/cm3", 2.0)
-material1.read_nuclides_from_json({"Be9": "tests/Be9.json", "Li6": "tests/Li6.json", "Li7": "tests/Li7.json"})
+    # Create materials with different absorption characteristics
+    material1 = mc.Material()
+    material1.material_id = 1  # Set material_id for MaterialFilter testing
+    material1.add_nuclide("Li6", 0.07/2)  # Li4SiO4
+    material1.add_nuclide("Li7", 0.93/2)
+    material1.add_nuclide("Be9", 0.5)
+    # material1.add_element("O", 4.0)
+    # material1.add_element("Si", 1.0)
+    material1.set_density("g/cm3", 2.0)
+    if code == 'materials_for_mc':
+        material1.read_nuclides_from_json({"Be9": "tests/Be9.json", "Li6": "tests/Li6.json", "Li7": "tests/Li7.json"})
 
 
-# Create cells
-cell1 = mc.Cell(
-    cell_id=1,
-    name="inner_sphere",
-    region=region1,
-)
-cell2 = mc.Cell(
-    cell_id=2,
-    name="outer_annular",
-    region=region2,
-    fill=material1,
-)
-geometry = mc.Geometry([cell1, cell2])
+    # Create cells
+    cell1 = mc.Cell(
+        cell_id=1,
+        name="inner_sphere",
+        region=region1,
+    )
+    cell2 = mc.Cell(
+        cell_id=2,
+        name="outer_annular",
+        region=region2,
+        fill=material1,
+    )
+    geometry = mc.Geometry([cell1, cell2])
 
-source = mc.IndependentSource(
-    space=[0.0, 0.0, 0.0],
-    angle=mc.stats.Isotropic(),
-    energy=14.06e6
-)
-settings = mc.Settings(
-    particles=50000,
-    batches=2,
-    source=source,
-    seed=1
-)
+    if code == 'openmc':
+        source = mc.IndependentSource(
+            space=mc.stats.Point([0,0,0]),
+            angle=mc.stats.Isotropic(),
+            energy=mc.stats.Discrete([14060000.0], [1.0])
+        )
+        settings = mc.Settings(
+            particles=50000,
+            batches=20,
+            source=source,
+            seed=1,
+            run_mode='fixed source'
+        )
+    elif code == 'materials_for_mc':
+        source = mc.IndependentSource(
+            space=[0.0, 0.0, 0.0],
+            angle=mc.stats.Isotropic(),
+            energy=14.06e6
+        )
+        settings = mc.Settings(
+            particles=50000,
+            batches=2,
+            source=source,
+            seed=1
+        )
 
-# Create tallies with CellFilters
-cell_filter2 = mc.CellFilter(cell2)
-tally1 = mc.Tally()
-tally1.filters = [cell_filter2]
-tally1.scores = [105]  # n,t
-tally1.name = "tbr"
-tallies = [tally1]
+    # Create tallies with CellFilters
+    cell_filter2 = mc.CellFilter(cell2)
+    tally1 = mc.Tally()
+    tally1.filters = [cell_filter2]
+    if code == 'openmc':
+        tally1.scores = ['105']  # n,t
+    elif code == 'materials_for_mc':
+        tally1.scores = [105]  # n,t
+    tally1.name = "tbr"
+    tallies = [tally1]
 
-model = mc.Model(geometry=geometry, settings=settings, tallies=tallies)
+    model = mc.Model(geometry=geometry, settings=settings, tallies=tallies)
 
-time.start = time.time()
-model.run()
-print(f"Simulation completed in {time.time() - time.start} seconds.")
-# Tallies are updated in place!
-print(f"TBR (tritium breeding ratio): {tally1.mean}")
+    time.start = time.time()
+    if code == 'openmc':
+        model.run(apply_tally_results=True)
+    elif code == 'materials_for_mc':
+        model.run()
+    print(f"Simulation completed in {time.time() - time.start} seconds.")
+    # Tallies are updated in place!
+    print(f"{code} TBR (tritium breeding ratio): {tally1.mean}")
